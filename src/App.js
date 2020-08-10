0ad72d86282106f201ea0437d7c31708d54a7210
vbudithi <vivekjohn.vivek904@gmail.com>
2020-08-10 13:54:18 +0930

    listing all the countries -  diseases.sh(website)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import{
+   MenuItem,
+   FormControl,
+   Select 
+} from "@material-ui/core";
+
+import "./App.css"
 
 function App() {
+  const [countries, setCountries] = useState([]);
+     
+  useEffect(() =>{
+          const getCountriesData = async () => {
+            await fetch("https://disease.sh/v3/covid-19/countries")
+            .then((response) => response.json())
+            .then((data) => {
+              const countries = data.map((country) => (
+                {
+                  name: country.country,
+                  value: country.countryInfo.iso2,
+                }
+              ));
+              setCountries(countries);
+            });
+          }
+          getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+      <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+           <Select
+              variant="outlined"
+              value="abc"
+            >
+              {
+                countries.map((country) => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+              {/*
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">1</MenuItem>
+              <MenuItem value="worldwide">2</MenuItem>
+              <MenuItem value="worldwide">3</MenuItem>  
+              */}
+           </Select>
+       </FormControl>          
+      </div>
+       
     </div>
   );
 }
