9fd6e25b07dc015ccfb66c55f0b7adba64dafb1a
vbudithi <vivekjohn.vivek904@gmail.com>
2020-08-11 16:07:44 +0930

    displaying countries list with cases

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from 'react';
 import{
    MenuItem,
    FormControl,
-   Select 
+   Select,
+   Card,
+   CardContent, 
 } from "@material-ui/core";
-
+import InfoBox from './InfoBox';
+import Map from "./Map";
+import Table from "./Table";
 import "./App.css"
 
 function App() {
-  const [countries, setCountries] = useState([]);
-     
+  const [ countries, setCountries ] = useState([]);
+  const [ country, setCountry ] = useState('worldwide');
+  const [ countryInfo, setCountryInfo ] = useState({});
+  const [ tableData, setTableData] = useState([]);
+
   useEffect(() =>{
           const getCountriesData = async () => {
             await fetch("https://disease.sh/v3/covid-19/countries")
@@ -21,38 +28,65 @@ function App() {
                   value: country.countryInfo.iso2,
                 }
               ));
+
+              setTableData(data);
               setCountries(countries);
             });
           }
           getCountriesData();
   }, []);
 
+   const onCountryChange = async(event) => {
+      const countryCode = event.target.value;
+      setCountry(countryCode);          
+
+      const url = countryCode === "worldwide" 
+          ? "https://disease.sh/v3/covid-19/all"
+          : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+        await fetch(url)
+        .then((response) => response.json())
+        .then((data) => { 
+            setCountry( countryCode );
+            setCountryInfo(data);
+        });
+      };
+      console.log('country info>>>' , countryInfo);
+
   return (
     <div className="app">
-      <div className="app__header">
-      <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app__dropdown">
-           <Select
-              variant="outlined"
-              value="abc"
-            >
-              {
-                countries.map((country) => (
-                  <MenuItem value={country.value}>{country.name}</MenuItem>
-                ))
-              }
-              {/*
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+            <FormControl className="app__dropdown">
+             <Select variant="outlined" value= { country } onChange={onCountryChange} >
               <MenuItem value="worldwide">Worldwide</MenuItem>
-              <MenuItem value="worldwide">1</MenuItem>
-              <MenuItem value="worldwide">2</MenuItem>
-              <MenuItem value="worldwide">3</MenuItem>  
-              */}
-           </Select>
-       </FormControl>          
-      </div>
-       
+                {
+                  countries.map((country) => (
+                    <MenuItem value={country.value}>{country.name}</MenuItem>
+                  ))
+                }
+             </Select>
+            </FormControl>          
+        </div>       
+        <div className="app__stats">
+          <InfoBox title="coronavirus cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+        </div>
+        <Map />
+      </div>     
+      <Card className="app__right">
+        <CardContent>
+          <h3> live  casees</h3>
+          <Table countries = {tableData} />
+        </CardContent>
+      </Card>
     </div>
-  );
+  
+  );  
 }
 
 export default App;
---
+++ src/InfoBox.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+function InfoBox({ title, cases, total }) {
+    return (
+        <Card className="infoBox">
+           <CardContent>
+               <Typography className="infoBox__title" color= "textSecondary">
+                    {title}
+               </Typography>
+                
+               <h2 className="infoBox__total" color="textSecondary">
+                   {total} Total
+               </h2>
+
+               <h4 className="infoBox__cases"> + {cases} cases today </h4>
+             
+           </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
+
+
